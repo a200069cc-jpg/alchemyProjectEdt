7e2bebf83675273ba9af767fb5c3a64841eddb2e
a241593kb <a241593kb@correu.escoladeltreball.org>
2025-12-03 17:38:45 +0100

    page 2.1 table finish

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {ListComponent} from './components/ListComponent'
+import { ListComponent } from './components/ListComponent'
 import { FilterComponent } from './components/FilterComponent';
+import { TableComponent } from './components/TableComponent';
 
 export default function App() {
   const PlaceHolderList = [
@@ -31,15 +32,7 @@ export default function App() {
     { id: '24', title: 'Elemento 24' },]
   return (
     <View style={styles.container}>
-      
-      <View style={styles.topContainer}>
-    
-      <ListComponent data={PlaceHolderList} />
-      </View>
-      <View style={styles.bottomContainer}>
-      <FilterComponent></FilterComponent>
-      <ListComponent data={PlaceHolderList} />
-      </View>
+    <TableComponent/>
      
     </View>
   );
